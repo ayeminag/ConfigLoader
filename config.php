90ace5899ebfd82385dbefb493c6ccf54f699f13
Aye Min Aung <mgdevine89@gmail.com>
2014-11-30 16:50:07 +0630

    changed property name

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 class Config{
   public $path;
-  private $configArray;
+  private $configValue;
   private $keys;
   public function __construct(){
     $this->path = __DIR__.DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR;
@@ -10,14 +10,14 @@ class Config{
 
   public function get($accessor){
     $this->keys = explode(".", $accessor);
-    $this->configArray = require $this->path.$this->keys[0].".php";
+    $this->configValue = require $this->path.$this->keys[0].".php";
     $this->resolveKeys();
-    return $this->configArray;
+    return $this->configValue;
   }
 
   private function resolveKeys(){
     for ($i = 1; $i < count($this->keys); $i++) {
-      $this->configArray = $this->configArray[$this->keys[$i]];
+      $this->configValue = $this->configValue[$this->keys[$i]];
     }
   }
 }
